Randomly select only one image per patch; Do not use all augmented data for training; Fix metrics calculation; Fix exploding gradients;

src/dataset.py
@@ -1,12 +1,9 @@
-import h5py
 from line_profiler import profile
 from torch.utils.data import Dataset
 import torch
 from torch import nn
-from PIL import Image
-import io
-import numpy as np
 from torchvision.transforms import v2
+from random import randrange
 
 # --- Constants for Patching ---
 IMAGE_PREPROCESSED_SIZE = 256
@@ -79,30 +76,21 @@ class ImageNetPatchDataset(Dataset):
                 f"ImageNetPatchDataset split argument expected to be one of {VALID_SPLITS}, got {split}"
             )
 
-        # new_hf_dataset = []
-        # for i, x in enumerate(hf_dataset):
-        #     new_hf_dataset.append(x)
-        #     if i >= 1000:
-        #         break
-        # self.hf_dataset = hf_dataset
         self.hdf5_dataset = hdf5_dataset
         self.split = split
         self.split_labels = f"{split}-labels"
-        # test = h5_images[1]
-        # print(test)
-        # self.h5_images = h5_images
-        # self.h5_labels = h5_labels
         self.num_original_images = len(self.hdf5_dataset[split])
 
     def __len__(self):
-        # return self.num_original_images * PATCHES_PER_ORIGINAL_IMAGE
         return self.num_original_images
 
     # @profile
     def __getitem__(self, idx):
         # Calculate which original image and which patch within that image to get
         original_image_idx = idx // PATCHES_PER_ORIGINAL_IMAGE
-        patch_idx_within_image = idx % PATCHES_PER_ORIGINAL_IMAGE
+        patch_idx_within_image = randrange(
+            PATCHES_PER_ORIGINAL_IMAGE
+        )  # Randomly select one Patch from the image
 
         original_image = torch.from_numpy(
             self.hdf5_dataset[self.split][original_image_idx]
@@ -111,10 +99,7 @@ class ImageNetPatchDataset(Dataset):
 
         selected_patch = get_single_patch(original_image, patch_idx_within_image)
 
-        label = nn.functional.one_hot(
-            torch.tensor(original_label, dtype=torch.long), num_classes=NUM_CLASSES
-        ).float()
-        return selected_patch, label
+        return selected_patch, torch.tensor(original_label, dtype=torch.long)
 
 
 class ImageNetHuggingFaceDataset(Dataset):

src/main.py
@@ -9,23 +9,16 @@ if os.getenv("HF_HOME") is None:
     sys.exit(1)
 import mlflow
 import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
 import torch
 import arguments
 from dataset import ImageNetPatchDataset
 import logs
 from lightning.pytorch.loggers import MLFlowLogger
-from model import AlexNet
+from model import AlexNet, alexnet_initialize_weights
 from torch.utils.data import DataLoader
-import optuna
-from datasets import load_dataset
 from pathlib import Path
-from torchvision.transforms import v2
-from PIL import Image
-from torchinfo import summary
 import h5py
-import cProfile
-import pstats
-import tqdm
 
 args = arguments.get_args()
 logger = logs.get_logger("train")
@@ -43,25 +36,10 @@ mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 logger.info(f"🤖 Using device: {device}")
 
-
-# Helper functions
-def get_dataloader(dataset, params):
-    dataset = ImageNetPatchDataset(dataset)
-    return DataLoader(
-        dataset,
-        batch_size=params["batch_size"],
-        num_workers=os.cpu_count() // 2,
-        shuffle=False,
-        pin_memory=True,
-        prefetch_factor=4,
-        in_order=False,
-    )
-
-
 config = {
     "batch_size": 128,  # Original paper: 128
-    "lr": 0.01,  # "We used an equal learning rate for all layers, which we adjusted manually throughout training. The heuristic which we followed was to divide the learning rate by 10 when the validation error rate stopped improving with the current learning rate."
-    "epochs": 1,  # Original paper: 90
+    "lr": 0.1,  # Original paper: 0.01  # "We used an equal learning rate for all layers, which we adjusted manually throughout training. The heuristic which we followed was to divide the learning rate by 10 when the validation error rate stopped improving with the current learning rate."
+    "epochs": 90,  # Original paper: 90
 }
 
 logger.info(f"☁️  Loading dataset...")
@@ -70,33 +48,55 @@ hdf5 = h5py.File(H5_DATASET_PATH, "r")
 train_dataloader = DataLoader(
     ImageNetPatchDataset(hdf5, "train"),
     batch_size=config["batch_size"],
-    num_workers=os.cpu_count() // 2,
-    shuffle=False,
+    num_workers=os.cpu_count() // 3,
+    shuffle=True,
     pin_memory=True,
-    prefetch_factor=4,
-    in_order=False,
+    # prefetch_factor=2,
+    # in_order=True,
 )
 
 val_dataloader = DataLoader(
     ImageNetPatchDataset(hdf5, "validation"),
     batch_size=config["batch_size"],
-    num_workers=os.cpu_count() // 2,
+    num_workers=os.cpu_count() // 3,
     shuffle=False,
     pin_memory=True,
-    prefetch_factor=4,
-    in_order=False,
+    # prefetch_factor=2,
+    # in_order=True,
 )
+
+# test_dataloader = DataLoader(
+#     ImageNetPatchDataset(hdf5, "test"),
+#     batch_size=config["batch_size"],
+#     num_workers=os.cpu_count() // 3,
+#     shuffle=False,
+#     pin_memory=True,
+#     # prefetch_factor=2,
+#     # in_order=True,
+# )
 
 mlf_logger = MLFlowLogger(
     experiment_name=MLFLOW_EXPERIMENT_NAME, tracking_uri=MLFLOW_URI
 )
 model = AlexNet(params=config)
+alexnet_initialize_weights(model)
+
+checkpoint_callback = ModelCheckpoint(
+    dirpath=Path(SAVE_MODEL_DIR),
+    every_n_epochs=1,
+    mode="min",
+    monitor="val_loss",
+    save_last=True,
+)
 
 trainer = L.Trainer(
     default_root_dir=Path(SAVE_MODEL_DIR),
+    callbacks=[checkpoint_callback],
     max_epochs=config["epochs"],
     logger=mlf_logger,
     enable_progress_bar=True,
+    gradient_clip_val=10.0,
+    gradient_clip_algorithm="norm",
     # profiler="simple",
     # profiler="advanced",
     # profiler="pytorch",

src/model.py
@@ -1,6 +1,7 @@
 import lightning as L
 import torch
 from torch import nn
+import torch.nn.init as init
 from torchmetrics.classification import (
     MulticlassF1Score,
     MulticlassAccuracy,
@@ -39,31 +40,31 @@ class AlexNet(L.LightningModule):
         self.save_hyperparameters()
         self.lr = params["lr"]
         self.model = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2.0),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(96, 256, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2.0),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(256, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),  # Index 0
+            nn.ReLU(inplace=True),  # Index 1
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2.0),  # Index 2
+            nn.MaxPool2d(kernel_size=3, stride=2),  # Index 3
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),  # Index 4
+            nn.ReLU(inplace=True),  # Index 5
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2.0),  # Index 6
+            nn.MaxPool2d(kernel_size=3, stride=2),  # Index 7
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),  # Index 8
+            nn.ReLU(inplace=True),  # Index 9
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),  # Index 10
+            nn.ReLU(inplace=True),  # Index 11
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),  # Index 12
+            nn.ReLU(inplace=True),  # Index 13
+            nn.MaxPool2d(kernel_size=3, stride=2),  # Index 14
             # Classification
-            nn.AdaptiveAvgPool2d((6, 6)),
-            nn.Flatten(),
-            nn.Dropout(p=0.5),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, NUM_CLASSES),
+            nn.AdaptiveAvgPool2d((6, 6)),  # Index 15
+            nn.Flatten(),  # Index 16
+            nn.Dropout(p=0.5),  # Index 17
+            nn.Linear(256 * 6 * 6, 4096),  # Index 18
+            nn.ReLU(inplace=True),  # Index 19
+            nn.Dropout(p=0.5),  # Index 20
+            nn.Linear(4096, 4096),  # Index 21
+            nn.ReLU(inplace=True),  # Index 22
+            nn.Linear(4096, NUM_CLASSES),  # Index 23
         )
 
         self.criterion = nn.CrossEntropyLoss()
@@ -71,8 +72,15 @@ class AlexNet(L.LightningModule):
         smoothing_factor = 0.18  # Around 10 datapoints
         self.loss_smoother = ExponentialMovingAverage(smoothing_factor)
         self.f1_metric = MulticlassF1Score(num_classes=NUM_CLASSES, average="macro")
-        self.accuracy_metric = MulticlassAccuracy(
-            num_classes=NUM_CLASSES, average="macro"
+        self.top1_accuracy_metric = MulticlassAccuracy(
+            num_classes=NUM_CLASSES,
+            average="macro",
+            top_k=1,
+        )
+        self.top5_accuracy_metric = MulticlassAccuracy(
+            num_classes=NUM_CLASSES,
+            average="macro",
+            top_k=5,
         )
         self.precision_metric = MulticlassPrecision(
             num_classes=NUM_CLASSES, average="macro"
@@ -80,8 +88,15 @@ class AlexNet(L.LightningModule):
 
         self.val_loss_smoother = ExponentialMovingAverage(smoothing_factor)
         self.val_f1_metric = MulticlassF1Score(num_classes=NUM_CLASSES, average="macro")
-        self.val_accuracy_metric = MulticlassAccuracy(
-            num_classes=NUM_CLASSES, average="macro"
+        self.val_top1_accuracy_metric = MulticlassAccuracy(
+            num_classes=NUM_CLASSES,
+            average="macro",
+            top_k=1,
+        )
+        self.val_top5_accuracy_metric = MulticlassAccuracy(
+            num_classes=NUM_CLASSES,
+            average="macro",
+            top_k=5,
         )
         self.val_precision_metric = MulticlassPrecision(
             num_classes=NUM_CLASSES, average="macro"
@@ -91,8 +106,15 @@ class AlexNet(L.LightningModule):
         self.test_f1_metric = MulticlassF1Score(
             num_classes=NUM_CLASSES, average="macro"
         )
-        self.test_accuracy_metric = MulticlassAccuracy(
-            num_classes=NUM_CLASSES, average="macro"
+        self.test_top1_accuracy_metric = MulticlassAccuracy(
+            num_classes=NUM_CLASSES,
+            average="macro",
+            top_k=1,
+        )
+        self.test_top5_accuracy_metric = MulticlassAccuracy(
+            num_classes=NUM_CLASSES,
+            average="macro",
+            top_k=5,
         )
         self.test_precision_metric = MulticlassPrecision(
             num_classes=NUM_CLASSES, average="macro"
@@ -105,42 +127,53 @@ class AlexNet(L.LightningModule):
         optimizer = torch.optim.SGD(
             self.parameters(), lr=self.lr, momentum=0.9, weight_decay=0.0005
         )
-        return optimizer
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
+                    optimizer,
+                    mode="max",
+                    factor=0.1,
+                    patience=5,
+                    min_lr=1e-5,
+                    cooldown=20,
+                ),
+                "interval": "epoch",
+                "monitor": "val_acc_top1",
+                "frequency": 1,
+            },
+        }
 
     # @profile
     def training_step(self, train_batch, batch_idx, *args, **kwargs):
         x, y = train_batch
 
         logits = self.model(x)
-
         loss = self.criterion(logits, y)
 
-        if batch_idx % 50 == 0:
+        predictions = torch.argmax(logits, dim=-1)
 
-            probabilities = torch.nn.functional.softmax(logits, dim=-1)
-            predictions = torch.argmax(probabilities, dim=-1)
-            predictions = torch.nn.functional.one_hot(
-                predictions, num_classes=NUM_CLASSES
-            )
+        loss_item = loss.item()
+        self.loss_smoother.update(loss_item)
+        self.f1_metric.update(predictions, y)
+        self.top1_accuracy_metric.update(logits, y)
+        self.top5_accuracy_metric.update(logits, y)
+        self.precision_metric.update(predictions, y)
 
-            item = loss.item()
-            self.loss_smoother.update(item)
-            self.f1_metric.update(predictions, y)
-            self.accuracy_metric.update(predictions, y)
-            self.precision_metric.update(predictions, y)
-
-            self.log("train_loss", loss)
-            self.log("train_loss_smoothed", self.loss_smoother.get_smoothed_value())
-            self.log("train_f1", self.f1_metric.compute().cpu().item())
-            self.log("train_acc", self.accuracy_metric.compute().cpu().item())
-            self.log("train_prec", self.precision_metric.compute().cpu().item())
+        self.log("train_loss", loss_item)
+        self.log("train_loss_smoothed", self.loss_smoother.get_smoothed_value())
+        self.log("train_f1", self.f1_metric.compute().cpu().item())
+        self.log("train_acc_top1", self.top1_accuracy_metric.compute().cpu().item())
+        self.log("train_acc_top5", self.top5_accuracy_metric.compute().cpu().item())
+        self.log("train_prec", self.precision_metric.compute().cpu().item())
 
         return loss
 
     def on_validation_start(self):
         self.val_loss_smoother.reset()
         self.val_f1_metric.reset()
-        self.val_accuracy_metric.reset()
+        self.val_top1_accuracy_metric.reset()
+        self.val_top5_accuracy_metric.reset()
         self.val_precision_metric.reset()
 
     def validation_step(self, val_batch, *args, **kwargs):
@@ -149,20 +182,22 @@ class AlexNet(L.LightningModule):
         logits = self.model(x)
         loss = self.criterion(logits, y)
 
-        probabilities = torch.nn.functional.softmax(logits, dim=-1)
-        predictions = torch.argmax(probabilities, dim=-1)
-        predictions = torch.nn.functional.one_hot(predictions, num_classes=NUM_CLASSES)
+        predictions = torch.argmax(logits, dim=-1)
 
-        self.val_loss_smoother.update(loss.item())
+        loss_item = loss.item()
+        self.val_loss_smoother.update(loss_item)
         self.val_f1_metric.update(predictions, y)
-        self.val_accuracy_metric.update(predictions, y)
+        self.val_top1_accuracy_metric.update(logits, y)
+        self.val_top5_accuracy_metric.update(logits, y)
         self.val_precision_metric.update(predictions, y)
 
-        self.log("val_loss", loss)
+        self.log("val_loss", loss_item)
         self.log("val_loss_smoothed", self.val_loss_smoother.get_smoothed_value())
         self.log("val_f1", self.val_f1_metric.compute().cpu().item())
-        self.log("val_acc", self.val_accuracy_metric.compute().cpu().item())
+        self.log("val_acc_top1", self.val_top1_accuracy_metric.compute().cpu().item())
+        self.log("val_acc_top5", self.val_top5_accuracy_metric.compute().cpu().item())
         self.log("val_prec", self.val_precision_metric.compute().cpu().item())
+
         return loss
 
     def test_step(self, test_batch, *args, **kwargs):
@@ -171,22 +206,57 @@ class AlexNet(L.LightningModule):
         logits = self.model(x)
         loss = self.criterion(logits, y)
 
-        probabilities = torch.nn.functional.softmax(logits, dim=-1)
-        predictions = torch.argmax(probabilities, dim=-1)
-        predictions = torch.nn.functional.one_hot(predictions, num_classes=NUM_CLASSES)
+        predictions = torch.argmax(logits, dim=-1)
 
-        self.test_loss_smoother.update(loss.item())
+        loss_item = loss.item()
+        self.test_loss_smoother.update(loss_item)
         self.test_f1_metric.update(predictions, y)
-        self.test_accuracy_metric.update(predictions, y)
+        self.test_top1_accuracy_metric.update(logits, y)
+        self.test_top5_accuracy_metric.update(logits, y)
         self.test_precision_metric.update(predictions, y)
 
-        self.log("test_loss", loss)
+        self.log("test_loss", loss_item)
         self.log("test_loss_smoothed", self.test_loss_smoother.get_smoothed_value())
         self.log("test_f1", self.test_f1_metric.compute().cpu().item())
-        self.log("test_acc", self.test_accuracy_metric.compute().cpu().item())
+        self.log("test_acc_top1", self.test_top1_accuracy_metric.compute().cpu().item())
+        self.log("test_acc_top5", self.test_top5_accuracy_metric.compute().cpu().item())
         self.log("test_prec", self.test_precision_metric.compute().cpu().item())
         return loss
 
     # @profile
     def backward(self, loss, *args, **kwargs):
         loss.backward()
+
+
+# --- Custom Initialization Function for AlexNet ---
+def alexnet_initialize_weights(alexnet: AlexNet):
+    """
+    Initializes weights and biases of an AlexNet model according to the original paper.
+    """
+    for m in alexnet.modules():
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+            # All weights from a zero-mean Gaussian distribution with standard deviation 0.01
+            init.normal_(m.weight, mean=0.0, std=0.01)
+            if m.bias is not None:
+                # Initialize all biases to 0 by default
+                init.constant_(m.bias, 0)
+
+    # Second convolutional layer
+    if alexnet.model[4].bias is not None:
+        init.constant_(alexnet.model[4].bias, 1)
+
+    # Fourth convolutional layer
+    if alexnet.model[10].bias is not None:
+        init.constant_(alexnet.model[10].bias, 1)
+
+    # Fifth convolutional layer
+    if alexnet.model[12].bias is not None:
+        init.constant_(alexnet.model[12].bias, 1)
+
+    # First fully-connected hidden layer
+    if alexnet.model[18].bias is not None:
+        init.constant_(alexnet.model[18].bias, 1)
+
+    # Second fully-connected hidden layer
+    if alexnet.model[21].bias is not None:
+        init.constant_(alexnet.model[21].bias, 1)